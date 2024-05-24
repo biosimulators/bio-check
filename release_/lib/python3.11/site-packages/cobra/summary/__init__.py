b5a6ec96013cd16751265ba441b38d14b5056c03
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-05-24 18:07:58 -0400

    rem files

--- release_/lib/python3.11/site-packages/cobra/summary/__init__.py
+++
@@ -1,4 +0,0 @@
-from .summary import Summary
-from .metabolite_summary import MetaboliteSummary
-from .reaction_summary import ReactionSummary
-from .model_summary import ModelSummary
--- release_/lib/python3.11/site-packages/cobra/util/__init__.py
+++
@@ -1,5 +0,0 @@
-from cobra.util.array import *
-from cobra.util.context import *
-from cobra.util.solver import *
-from cobra.util.util import *
-from cobra.util.process_pool import *
