b18fafbb24ce260f8f94e09c3360809ab405bb76
alexPatrie <alexanderpatrie@gmail.com>
2024-06-03 18:29:44 -0400

    chore: savepoint

--- server/data_model.py
+++ server/data_model.py
@@ -1,9 +1,17 @@
 from typing import *
 
-from pydantic import BaseModel
+from pydantic import BaseModel as _BaseModel, ConfigDict
+
+
+class BaseModel(_BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
 
 class ArchiveUploadResponse(BaseModel):
     filename: str
 
+
+class UtcSpeciesComparison(BaseModel):
+    mse: Dict
+    proximity: Dict
 
--- server/main.py
+++ server/main.py
@@ -1,12 +1,14 @@
 import tempfile
+from typing import *
 
 import uvicorn
-from fastapi import FastAPI, UploadFile, File
+from pydantic import Field
+from fastapi import FastAPI, UploadFile, File, Body
 from fastapi.middleware.cors import CORSMiddleware
 
 from server.handlers.io import save_omex_archive
 from server.handlers.compare import generate_utc_species_comparison
-from server.data_model import ArchiveUploadResponse
+from server.data_model import ArchiveUploadResponse, UtcSpeciesComparison
 
 
 app = FastAPI(title='verification-service')
@@ -24,15 +26,39 @@ app.add_middleware(
 )
 
 
+# @app.post(
+#     "/upload-omex",
+#     response_model=ArchiveUploadResponse,
+#     summary='Upload an OMEX archive')
+# async def upload_omex(file: UploadFile = File(...), save_location: str = Field(...)) -> ArchiveUploadResponse:
+#     contents = await file.read()
+#     save_dir = save_location or tempfile.mkdtemp()
+#     archive_response = save_omex_archive(contents, save_dir)
+#     return ArchiveUploadResponse(filename=archive_response['source'])
+
+
 @app.post(
-    "/upload-omex",
-    response_model=ArchiveUploadResponse,
-    summary='Upload an OMEX archive')
-async def upload_omex(file: UploadFile = File(...)):
+    "/utc-species-comparison",
+    response_model=UtcSpeciesComparison,
+    summary="Compare UTC outputs for a given species name")
+async def utc_species_comparison(
+        species_id: str,
+        save_location: str = None,
+        omex_fp: str = None,
+        file: UploadFile = File(...),
+        simulators: List[str] = Body(default=['amici', 'copasi', 'tellurium']),
+) -> UtcSpeciesComparison:
     contents = await file.read()
-    save_dir = tempfile.mkdtemp()
-    archive_response = download_file(contents, save_dir)
-    return ArchiveUploadResponse(filename=archive_response['source'])
+    save_dir = save_location or tempfile.mkdtemp()
+    archive_response = save_omex_archive(contents, save_dir)
+    comparison = generate_utc_species_comparison(
+        omex_fp=omex_fp,
+        out_dir=save_dir,
+        species_name=species_id,
+        simulators=simulators)
+
+    print(comparison)
+    return UtcSpeciesComparison(mse=comparison['mse'], proximity=comparison['prox'])
 
 
 if __name__ == "__main__":
