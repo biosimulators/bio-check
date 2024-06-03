c1fd298828cc78cc77f515d6c486b155314e43fa
alexPatrie <alexanderpatrie@gmail.com>
2024-06-03 16:10:35 -0400

    fix: updated src data

---
+++ server/handlers/output_data.py
@@ -0,0 +1,61 @@
+import os
+from typing import *
+from importlib import import_module
+
+import pandas as pd
+import numpy as np
+from process_bigraph import pp
+
+from biosimulator_processes.api.compare import generate_comparison
+from biosimulator_processes.io import read_report_outputs, standardize_report_outputs
+from biosimulators_amici.core import exec_sedml_docs_in_combine_archive as exec_amici
+from biosimulators_copasi.core import exec_sedml_docs_in_combine_archive as exec_copasi
+from biosimulators_tellurium.core import exec_sedml_docs_in_combine_archive as exec_tellurium
+from biosimulators_utils.report.data_model import ReportFormat
+from biosimulators_utils.config import Config
+
+
+def generate_biosimulator_outputs(omex_fp: str, output_root_dir: str, simulators: list[str] = None) -> dict:
+    if not os.path.exists(output_root_dir):
+        os.mkdir(output_root_dir)
+
+    output_data = {}
+    sims = simulators or ['amici', 'copasi', 'tellurium']
+    for sim in sims:
+        module = import_module(name=f'biosimulators_{sim}.core')
+        exec_func = getattr(module, 'exec_sedml_docs_in_combine_archive')
+
+        sim_output_dir = os.path.join(output_root_dir, f'{sim}_outputs')
+        if not os.path.exists(sim_output_dir):
+            os.mkdir(sim_output_dir)
+
+        sim_config = Config(LOG=False)
+        exec_func(archive_filename=omex_fp, out_dir=sim_output_dir, config=sim_config)
+        report_path = os.path.join(sim_output_dir, 'reports.h5')
+        sim_data = read_report_outputs(report_path)
+        output_data[sim] = sim_data
+
+    return output_data
+
+
+def exec_species_comparison(omex_fp: str, species_name: str, output_root_dir: str, simulators: list[str] = None):
+    spec_index = 0
+    simulator_outputs = generate_biosimulator_outputs(omex_fp, output_root_dir, simulators)
+    for i, spec_name in enumerate(list(simulator_outputs['amici'].keys())):
+        if species_name.lower() in spec_name.lower():
+            spec_index += i
+
+    outs = [simulator_outputs['amici'].data[spec_index].data, simulator_outputs['copasi'].data[spec_index].data, simulator_outputs['tellurium'].data[spec_index].data]
+    simulator_names = list(simulator_outputs.keys())
+    simulator_names.append('ground_truth')
+
+    ground_truth_report_fp = os.path.join(omex_source_dir, omex_name.replace('.omex', ''), 'reports.h5')
+    ground_truth_results = standardize_report_outputs(ground_truth_report_fp)
+    ground_truth = ground_truth_results['floating_species']['LacI protein']
+    outs.append(ground_truth)
+    outs_df = pd.DataFrame(data=np.array(outs).transpose(), columns=simulator_names)
+
+    return outs_df
+
+
+
