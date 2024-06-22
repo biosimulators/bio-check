756d7b2e4fe915d6a2bd998d27c095f2efc9fc54
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-06-22 13:03:11 -0400

    chore: update

--- verification_service/data_model/worker.py
+++ verification_service/data_model/worker.py
@@ -8,10 +8,19 @@ from verification_service.data_model.shared import BaseClass, BaseModel, DbConne
 
 @dataclass
 class Worker(BaseClass):
-    pending_job_ids: List[str]
-    in_progress_job_ids: List[str]
-    completed_job_ids: List[str]
     db_connector: DbConnector
+    pending_job_ids: List[str] = None
+    in_progress_job_ids: List[str] = None
+    completed_job_ids: List[str] = None
+
+    def __post_init__(self):
+        id_key = 'job_id'
+        coll_names = ['completed_jobs', 'in_progress_jobs', 'pending_jobs']
+        job_ids = dict(zip(
+            coll_names,
+            [[job[id_key] for job in self.db_connector[coll_name].find()] for coll_name in coll_names]
+        ))
+        self.completed_job_ids = [job[id_key] for job in self.db_connector.db['completed_jobs'].find()]
 
 
 class InProgressJob(Job):
