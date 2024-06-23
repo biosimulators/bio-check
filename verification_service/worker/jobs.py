2832d33507c21cb7ce341bd6cc19c9cccf541f75
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-06-23 11:32:28 -0400

    chore: update

--- verification_service/worker/jobs.py
+++ verification_service/worker/jobs.py
@@ -1,3 +1,5 @@
+# -- This should serve as the main file for worker container -- #
+
 import os
 import uuid
 from types import NoneType
@@ -35,10 +37,7 @@ mongo_client = MongoClient(MONGO_URI)
 db_connector = MongoDbConnector(client=mongo_client, database_id=DB_NAME)
 
 
-def jobid(): return str(uuid.uuid4())
-
-
-
+# -- WORKER: "Toolkit" => Has all of the tooling necessary to process jobs.
 
 @dataclass
 class Worker(BaseClass):
@@ -194,6 +193,7 @@ class Worker(BaseClass):
         return np.allclose(arr1, arr2, rtol=rTol, atol=aTol)
 
 
+# -- SUPERVISOR: "Clearance" => All resources needed for db & file-storage access. Can act on behalf of the entity.
 @dataclass
 class Supervisor(BaseClass):
     """
@@ -230,17 +230,15 @@ class Supervisor(BaseClass):
             [[job for job in self.db_connector.db[coll_name].find()] for coll_name in coll_names])
         )
 
-
-
     def initialize(self):
         # activate job queue
-        run = True
         while True:
             self.__setattr__('job_queue', self.check_jobs())
             if not len(self.job_queue['pending_jobs']):
-                run = False
+                break
             else:
-                cascading_load_arrows(self.queue_timer)  # sleep(self.queue_timer)
+                cascading_load_arrows(self.queue_timer)
+                # sleep(self.queue_timer)
 
     def _job_exists(self, collection_name: str, comparison_id: str):
         unique_id_query = {'comparison_id': comparison_id}
@@ -258,10 +256,10 @@ class Supervisor(BaseClass):
             for job in jobs_to_complete:
                 # get the next job in the queue based on the preferred_queue_index
                 job_id = jobs_to_complete.pop(preferred_queue_index)
-                job_doc = self.db_connector.pending_jobs.find_one({'job_id': job_id})
+                job_doc = self.db_connector.db.pending_jobs.find_one({'job_id': job_id})
                 job_comparison_id = job_doc['comparison_id']
                 unique_id_query = {'comparison_id': job_comparison_id}
-                in_progress_job = self.db_connector.db['in_progress_jobs'].find_one(unique_id_query) or None
+                in_progress_job = self.db_connector.db.in_progress_jobs.find_one(unique_id_query) or None
 
                 job_exists = partial(self._job_exists, comparison_id=job_comparison_id)
                 # case: the job (which has a unique comparison_id) has not been picked up and thus no in-progress job for the given comparison id yet exists
@@ -280,7 +278,7 @@ class Supervisor(BaseClass):
                 if not job_exists('completed_jobs'):
                     # pop in-progress job from internal queue and use it parameterize the worker
                     in_prog_id = in_progress_jobs.pop(preferred_queue_index)
-                    in_progress_doc = self.db_connector.in_progress_jobs.find_one({'job_id': in_prog_id})
+                    in_progress_doc = self.db_connector.db.in_progress_jobs.find_one({'job_id': in_prog_id})
                     workers_id = in_progress_doc['worker_id']
                     worker = self.call_worker(job_params=job_doc, worker_id=workers_id)
 
@@ -298,197 +296,3 @@ class Supervisor(BaseClass):
 
     def call_worker(self, job_params: Dict, worker_id: Optional[str] = None) -> Worker:
         return Worker(job_params=job_params, worker_id=worker_id)
-
-
-
-async def exec_utc_comparison(
-        omex_path: str,
-        simulators: List[str],
-        include_outputs: bool = True,
-        comparison_id: str = None,
-        ground_truth_report_path: str = None
-        ) -> Union[UtcComparison, SimulationError]:
-    """Execute a Uniform Time Course comparison for ODE-based simulators from Biosimulators."""
-    result = await utc_comparison(
-        omex_path=omex_path,
-        simulators=simulators,
-        include_outputs=include_outputs,
-        comparison_id=comparison_id,
-        ground_truth_report_path=ground_truth_report_path
-    )
-    return result
-
-
-async def check_jobs():
-    jobs = [job for job in db_connector.db['pending_jobs'].find()]
-
-    jobs_to_process = []
-    while len(jobs) > 0:
-        # populate the queue of jobs with params to be processed
-        job = jobs.pop(0)
-        jobs_to_process.append(job)
-
-        comparison_id = job['comparison_id']
-
-        # check if in progress and mark the job in progress before handing it off if not
-
-        in_progress_coll = db_connector.get_collection("in_progress_jobs")
-        in_progress_job = in_progress_coll.find_one({'comparison_id': comparison_id})
-
-        if isinstance(in_progress_job, NoneType):
-            in_progress_job_id = jobid()
-            in_progress_doc = db_connector.insert_in_progress_job(in_progress_job_id, job['comparison_id'])
-            print(f"Successfully marked document IN_PROGRESS:\n{in_progress_doc}")
-
-            # TODO: Make this work with storage.
-            # job_result: UtcComparison = await utc_comparison(
-            #     omex_path=job['omex_path'],
-            #     simulators=job['simulators'],
-            #     comparison_id=comparison_id,
-            # )
-        else:
-            print(f"In progress job already exists: {in_progress_job}")
-
-        completed_id = jobid()
-        completed_doc = db_connector.insert_completed_job(
-            job_id=completed_id,
-            comparison_id=in_progress_job['comparison_id'],
-            # results=job_result.model_dump())
-            results={"a": [1, 2, 4]})  # here result would be that generated in job result
-
-        # rest to check
-        from asyncio import sleep
-        print(f"Sleeping for {5}...zzzzzzz...")
-        await sleep(5)
-    return {"status": "all jobs completed."}
-
-# @app.post(
-#     "/utc-comparison",
-#     response_model=UtcComparison,
-#     name="Uniform Time Course Comparison.",
-#     operation_id="utc-comparison",
-#     summary="Compare UTC outputs for each species in a given model file. You may pass either a model file or OMEX archive file.")
-# async def utc_comparison(
-#         uploaded_file: UploadFile = File(...),
-#         simulators: List[str] = Query(default=['amici', 'copasi', 'tellurium']),
-#         include_outputs: bool = Query(default=True),
-#         comparison_id: str = Query(default=None),
-#         # ground_truth: List[List[float]] = None,
-#         # time_course_config: Dict[str, Union[int, float]] = Body(default=None)
-# ) -> UtcComparison:
-#     out_dir = tempfile.mkdtemp()
-#     save_dir = tempfile.mkdtemp()
-#     omex_path = await save_uploaded_file(uploaded_file, save_dir)
-#     comparison_name = comparison_id or f'api-generated-utc-comparison-for-{simulators}'
-#     # generate async comparison
-#     comparison = generate_utc_comparison(
-#         omex_fp=omex_path,
-#         simulators=simulators,
-#         include_outputs=include_outputs,
-#         comparison_id=comparison_name)
-#     spec_comparisons = []
-#     for spec_name, comparison_data in comparison['results'].items():
-#         species_comparison = UtcSpeciesComparison(
-#             mse=comparison_data['mse'],
-#             proximity=comparison_data['prox'],
-#             output_data=comparison_data.get('output_data'),
-#             species_name=spec_name)
-#         spec_comparisons.append(species_comparison)
-#     return UtcComparison(results=spec_comparisons, id=comparison_name, simulators=simulators)
-
-
-# @app.post(
-#     "/utc-comparison",  # "/biosimulators-utc-comparison",
-#     response_model=UtcComparison,
-#     name="Biosimulator Uniform Time Course Comparison",
-#     operation_id="biosimulators-utc-comparison",
-#     summary="Compare UTC outputs from Biosimulators for a model from a given archive.")
-# async def utc_comparison(
-#         uploaded_file: UploadFile = File(..., description="OMEX/COMBINE Archive File."),
-#         simulators: List[str] = Query(
-#             default=['amici', 'copasi', 'tellurium'],
-#             description="Simulators to include in the comparison."
-#         ),
-#         include_outputs: bool = Query(
-#             default=True,
-#             description="Whether to include the output data on which the comparison is based."
-#         ),
-#         comparison_id: str = Query(
-#             default=None,
-#             description="Descriptive identifier for this comparison."
-#         ),
-#         ground_truth_report: UploadFile = File(
-#             default=None,
-#             description="reports.h5 file defining the so-called ground-truth to be included in the comparison.")
-#         ) -> UtcComparison:
-#
-#     try:
-#         save_dir = tempfile.mkdtemp()
-#         out_dir = tempfile.mkdtemp()
-#         omex_path = await save_uploaded_file(uploaded_file, save_dir)
-#
-#         if ground_truth_report is not None:
-#             report_filepath = await save_uploaded_file(ground_truth_report, save_dir)
-#             ground_truth = await read_report_outputs(report_filepath)
-#             truth_vals = ground_truth.to_dict()['data']
-#             # d = [d.to_dict() for d in ground_truth.data if "time" not in d.dataset_label.lower()]
-#             # truth_vals = [data['data'].tolist() for data in d]
-#         else:
-#             truth_vals = None
-#
-#         comparison_id = comparison_id or 'biosimulators-utc-comparison'
-#         comparison = await generate_biosimulators_utc_comparison(
-#             omex_fp=omex_path,
-#             out_dir=out_dir,  # TODO: replace this with an s3 endpoint.
-#             simulators=simulators,
-#             comparison_id=comparison_id,
-#             ground_truth=truth_vals)
-#
-#         spec_comparisons = []
-#         for spec_name, comparison_data in comparison['results'].items():
-#             species_comparison = UtcSpeciesComparison(
-#                 mse=comparison_data['mse'],
-#                 proximity=comparison_data['prox'],
-#                 output_data=comparison_data.get('output_data') if include_outputs else {},
-#                 species_name=spec_name)
-#             spec_comparisons.append(species_comparison)
-#     except SimulationError as e:
-#         raise HTTPException(status_code=400, detail=str(e.message))
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-#
-#     return UtcComparison(
-#         results=spec_comparisons,
-#         id=comparison_id,
-#         simulators=simulators)
-
-
-# @app.post(
-#     "/biosimulators-utc-species-comparison",
-#     response_model=UtcSpeciesComparison,
-#     summary="Compare UTC outputs from Biosimulators for a given species name")
-# async def biosimulators_utc_species_comparison(
-#         uploaded_file: UploadFile = File(...),
-#         species_id: str = Query(...),
-#         simulators: List[str] = Query(default=['amici', 'copasi', 'tellurium']),
-#         include_outputs: bool = Query(default=True)
-# ) -> UtcSpeciesComparison:
-#     # handle os structures
-#     save_dir = tempfile.mkdtemp()
-#     out_dir = tempfile.mkdtemp()
-#     omex_path = os.path.join(save_dir, uploaded_file.filename)
-#     with open(omex_path, 'wb') as file:
-#         contents = await uploaded_file.read()
-#         file.write(contents)
-#     # generate async comparison
-#     comparison = await generate_biosimulators_utc_species_comparison(
-#         omex_fp=omex_path,
-#         out_dir=out_dir,  # TODO: replace this with an s3 endpoint.
-#         species_name=species_id,
-#         simulators=simulators)
-#     out_data = comparison['output_data'] if include_outputs else None
-#     return UtcSpeciesComparison(
-#         mse=comparison['mse'],
-#         proximity=comparison['prox'],
-#         output_data=out_data,
-#         species_name=species_id)
