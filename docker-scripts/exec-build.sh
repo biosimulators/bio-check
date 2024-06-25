2d288aded6763869eae4e62d894b3aef09abf48b
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-06-25 10:24:16 -0400

    chore: savepoint

--- docker-scripts/exec-build.sh
+++ docker-scripts/exec-build.sh
@@ -2,12 +2,7 @@
 
 lib="$1"
 
-if [ "${lib}" == "" ]; then
-  echo "You must enter one of: api, storage, worker library names as a runtime arg."
-  exit 1
-fi
-
 docker system prune -a -f
-cd ../verification_service/$lib
+cd ./verification_service/$lib
 
 docker build -t spacebearamadeus/verification-service-$lib .
--- docker-scripts/exec-run.sh
+++ docker-scripts/exec-run.sh
@@ -2,7 +2,7 @@
 
 lib="$1"
 
-if [ "${lib}" == "" ]; then
+if [ $lib == "" ]; then
   echo "You must enter one of: api, storage, worker library names as a runtime arg."
   exit 1
 fi
