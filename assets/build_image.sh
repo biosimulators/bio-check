db708dd6f181779dbec88dd234b2830f58fb1728
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-06-26 18:20:49 -0400

    minor update

--- assets/build_image.sh
+++ assets/build_image.sh
@@ -2,23 +2,23 @@
 
 # Args:
 # $1: the library for which you would like to build a container
-# -p ($2): whether to prune docker system before building anything.
-# -r ($3): whether to run the library container after build
+# --run ($2): whether to run the library container after build
+# --prune ($3): whether to prune docker system before building anything.
 # Run at root of repo!
 
 lib="$1"
-run_="$2" # -r
-prune="$3"  # -p
+run_="$2" # --run
+prune="$3"  # --prune
 
 
-if [ "$prune" ]; then
+if [ "$prune" == "--prune" ]; then
   yes | docker system prune -a
 fi
 
 cd bio_check/"$lib" || exit
 docker build -t ghcr.io/biosimulators/bio-check-"$lib" .
 
-if [ "$run_" ]; then
+if [ "$run_" == "--run" ]; then
   echo "Running container for $lib"
   ./run_container.sh "$lib"
 fi
