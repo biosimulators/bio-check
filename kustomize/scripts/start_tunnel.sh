6478e6c964de292106cd044ac69d5728114b8273
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-06-26 14:19:06 -0400

    feat: updated tunnel script

--- kustomize/scripts/start_tunnel.sh
+++ kustomize/scripts/start_tunnel.sh
@@ -1,4 +1,28 @@
 #!/usr/bin/env bash
 
-# start tunnel
-sudo minikube tunnel
+set -e 
+
+function start_minikube {
+    if minikube start --base-image gcr.io/k8s-minikube/kicbase-builds:v0.0.42-1703092832-17830 --driver docker  --memory=32g --cpus=8; then
+        echo "Minikube successfully started!"
+    fi 
+}
+
+function start_tunnel {
+    # start minikube
+    if minikube status | grep Stopped; then 
+        start_minikube 
+    else 
+        echo "Minikube has already started."
+    fi 
+
+    # start prometheus monitoring
+    helm install prometheus --namespace monitoring prometheus-community/kube-prometheus-stack
+
+    # start minikube tunnel
+    sudo minikube tunnel
+}
+
+start_tunnel
+
+# to stop: minikube stop
