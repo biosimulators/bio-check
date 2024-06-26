b92ea075f2e20195e54e6892231b0eb86b095ac9
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-06-26 15:29:24 -0400

    chore: update for save

--- kustomize/scripts/start_tunnel.sh
+++ kustomize/scripts/start_tunnel.sh
@@ -1,28 +1,20 @@
 #!/usr/bin/env bash
 
-set -e 
+status=$(minikube status | grep "Running")
+if [ ! -z $status ]; then 
+    minikube stop
+fi
 
-function start_minikube {
-    if minikube start --base-image gcr.io/k8s-minikube/kicbase-builds:v0.0.42-1703092832-17830 --driver docker  --memory=32g --cpus=8; then
-        echo "Minikube successfully started!"
-    fi 
-}
+minikube start --base-image gcr.io/k8s-minikube/kicbase-builds:v0.0.42-1703092832-17830 --driver docker  --memory=32g --cpus=8
+minikube addons enable metrics-server
 
-function start_tunnel {
-    # start minikube
-    if minikube status | grep Stopped; then 
-        start_minikube 
-    else 
-        echo "Minikube has already started."
-    fi 
+# start prometheus monitoring
+if helm install prometheus --namespace monitoring prometheus-community/kube-prometheus-stack; then
+    echo "Prometheus is set up!"
+else 
+    echo "Prometheus still in use"
+fi 
 
-    # start prometheus monitoring
-    helm install prometheus --namespace monitoring prometheus-community/kube-prometheus-stack
 
-    # start minikube tunnel
-    sudo minikube tunnel
-}
-
-start_tunnel
-
-# to stop: minikube stop
+# start minikube tunnel
+sudo minikube tunnel
