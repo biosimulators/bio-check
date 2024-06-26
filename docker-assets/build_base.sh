ecd58b3debeb84bc89c49a453ae4cac073a1f8c6
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-06-26 16:39:53 -0400

    chore

--- docker-assets/build_base.sh
+++ docker-assets/build_base.sh
@@ -1,5 +1,10 @@
 # Run at root of repo!
 
+prune="$1"  # -p
+if [ "$prune" ]; then
+  docker system prune -a -f
+fi
+
 echo "Building base image..."
 sudo docker build -t ghcr.io/biosimulators/bio-check-base .
 echo "Built base image."
