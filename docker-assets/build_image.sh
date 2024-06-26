07778fdb41b90d1b3c87e8b95df1078f7299ec06
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-06-26 17:55:15 -0400

    minor update

--- docker-assets/build_image.sh
+++ docker-assets/build_image.sh
@@ -3,21 +3,15 @@
 # Args:
 # $1: the library for which you would like to build a container
 # -p ($2): whether to prune docker system before building anything.
-# -b ($3): whether to build the base image before building library
-# -r ($4): whether to run the library container after build
+# -r ($3): whether to run the library container after build
 # Run at root of repo!
 
 lib="$1"
 prune="$2"  # -p
-build_="$3"  # -b
 run_="$4"
 
 if [ "$prune" ]; then
-  docker system prune -a -f
-fi
-
-if [ "$build_" ]; then
-  ./build_base.sh
+  yes | docker system prune -a
 fi
 
 cd bio_check/"$lib" || exit
