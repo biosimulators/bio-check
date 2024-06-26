ef2fbc816ef2d92e9333d6b090a91bbc4137d9ee
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-06-26 19:57:34 -0400

    updated again

--- bio_check/io.py
+++ bio_check/io.py
@@ -20,30 +20,3 @@ def make_dir(fp: str):
         os.mkdir(fp)
 
 
-async def read_report_outputs_async(report_file_path: str) -> BiosimulationsRunOutputData:
-    return read_report_outputs(report_file_path)
-
-
-def read_report_outputs(report_file_path) -> BiosimulationsRunOutputData:
-    """Read the outputs from all species in the given report file from biosimulations output.
-        Args:
-            report_file_path (str): The path to the simulation.sedml/report.h5 HDF5 file.
-    """
-    # TODO: implement auto gen from run id here.
-    outputs = []
-    with h5py.File(report_file_path, 'r') as f:
-        k = list(f.keys())
-        group_path = k[0] + '/report'
-        if group_path in f:
-            group = f[group_path]
-            dataset_labels = group.attrs['sedmlDataSetLabels']
-            for label in dataset_labels:
-                dataset_index = list(dataset_labels).index(label)
-                data = group[()]
-                specific_data = data[dataset_index]
-                output = BiosimulationsReportOutput(dataset_label=label, data=specific_data)
-                outputs.append(output)
-            return BiosimulationsRunOutputData(report_path=report_file_path, data=outputs)
-        else:
-            print(f"Group '{group_path}' not found in the file.")
-
