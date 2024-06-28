1a9a71e671bfd60c910b1ae43ff5720e8831f99a
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-06-28 08:44:16 -0400

    save update for no-install recs

--- assets/scripts/build.sh
+++ assets/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export PKG_ROOT=ghcr.io/biosimulators/bio-check
-version="$1"
+lib_version="$1"
 base_version="$2"
 
 # clear system
@@ -10,8 +10,11 @@ yes | docker system prune -a
 # build base
 docker build -f ./Dockerfile-base -t "$PKG_ROOT"-base:"$base_version" .
 
+# tag latest for base
+docker tag "$PKG_ROOT"-base:"$base_version" "$PKG_ROOT"-base:latest
+
 # build api
-docker build -f bio_check/api/Dockerfile-api -t "$PKG_ROOT"-api:"$version" ./bio_check/api
+docker build -f bio_check/api/Dockerfile-api -t "$PKG_ROOT"-api:"$lib_version" ./bio_check/api
 
 # build worker
-docker build -f bio_check/worker/Dockerfile-worker -t "$PKG_ROOT"-worker:"$version" ./bio_check/worker
+docker build -f bio_check/worker/Dockerfile-worker -t "$PKG_ROOT"-worker:"$lib_version" ./bio_check/worker
