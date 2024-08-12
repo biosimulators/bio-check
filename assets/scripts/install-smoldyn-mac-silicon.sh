8291a44ac920d19645dfb1a4603e216c22cee7c6
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-08-12 17:31:56 -0400

    updates for smoldyn

---
+++ assets/scripts/install-smoldyn-mac-silicon.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+# The following script serves as a utility for installing this repository with the Smoldyn requirement on a Silicon Mac
+
+# set installation parameters
+dist_url=https://www.smoldyn.org/smoldyn-2.73-mac.tgz
+tarball_name=smoldyn-2.73-mac.tgz
+dist_dir=${tarball_name%.tgz}
+
+# uninstall existing version
+pip uninstall smoldyn || return
+
+# download the appropriate distribution from smoldyn
+wget $dist_url
+
+# extract the source from the tarball
+tar -xzvf $tarball_name
+
+# delete the tarball
+rm $tarball_name
+
+# install smoldyn from the source
+cd $dist_dir || return
+
+if sudo -H ./install.sh; then
+  cd ..
+  # remove the smoldyn dist
+  rm -r $dist_dir
+  echo "Smoldyn successfully installed. Done."
+else
+  echo "Could not install smoldyn"
+fi
