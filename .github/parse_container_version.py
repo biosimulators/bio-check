38ac36b56504a20867d379c196bcbc9ff1a04147
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-09-13 13:47:33 -0400

    updated workflow

---
+++ .github/parse_container_version.py
@@ -0,0 +1,16 @@
+import yaml
+import sys
+
+
+service_name = sys.argv[1]
+fp = '../docker-compose.yaml'
+
+with open(fp, 'r') as file:
+    compose_data = yaml.safe_load(file)
+
+image = compose_data['services'][service_name]['image']
+version = image.split(":")[1]
+
+print(version)
+
+
