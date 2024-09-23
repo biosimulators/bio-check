38474e909852f5ad149c31bae83a901977e3236e
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-09-23 15:18:25 -0400

    update for re release

--- assets/scripts/build.sh
+++ assets/scripts/build.sh
@@ -1,23 +1,46 @@
 #!/usr/bin/env bash
 
-export PKG_ROOT=ghcr.io/biosimulators/bio-check
-lib_version="$1"
-base_version="$2"
+# TO BE RUN AT REPO ROOT
+# args = -a (build/push all) OR compose_api-OR-compose_worker
+# kwargs = -d (deploy to k8 after push)
+
+arg="$1"
+deploy="$2"
+
+set -e
 
 # clear system
-yes | docker system prune -a
+yes | docker system prune -a && yes | docker buildx prune -a
 
-# build base
-docker build -f ./Dockerfile-base -t "$PKG_ROOT"-base:"$base_version" .
+# clear pycaches
+python3 ./assets/scripts/rm_pycache.py
 
-# tag latest for base
-docker tag "$PKG_ROOT"-base:"$base_version" "$PKG_ROOT"-base:latest
+# build/push images
+if [ "$arg" != "-a" ]; then
+  # build/push single image
+  lib=arg
+  docker compose build "$lib" --no-cache
+  version=$(python3 ./.github/parse_container_version.py "$lib")
+  ./assets/scripts/push_image.sh "$lib" "$version" AlexPatrie
+else
+  # build/push all
+  docker compose build --no-cache
+  echo "Docker images built!"
 
-# build api
-docker build -f bio_check/api/Dockerfile-api -t "$PKG_ROOT"-api:"$lib_version" ./bio_check/api
+  api_version=$(python3 ./.github/parse_container_version.py compose_api)
+  echo "Using API version: $api_version"
 
-# build worker
-docker build -f bio_check/worker/Dockerfile-worker -t "$PKG_ROOT"-worker:"$lib_version" ./bio_check/worker
+  worker_version=$(python3 ./.github/parse_container_version.py compose_worker)
+  echo "Using worker version: $worker_version"
 
-# test run
-# docker run --name base --platform linux/amd64 "$PKG_ROOT"-base:"$base_version" /usr/bin/env bash
+  ./assets/scripts/push_image.sh compose_api "$api_version" AlexPatrie
+  ./assets/scripts/push_image.sh compose_api "$worker_version" AlexPatrie
+fi
+
+# optionally deploy to k8
+if [ "$deploy" == "-d" ]; then
+  cd kustomize \
+  && kubectl kustomize overlays/biochecknet | kubectl apply -f - \
+  && cd ..
+  echo "Overlays applied!"
+fi
