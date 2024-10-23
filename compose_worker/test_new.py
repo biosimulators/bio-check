7da5322e521c5f664d59bc44e1f37ddd939cca35
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-10-23 15:51:19 -0400

    another savepoint in the refactor of base, api, and worker deps

--- compose_worker/test_new.py
+++ compose_worker/test_new.py
@@ -2,6 +2,17 @@ import os
 
 import biosimulators_pysces as bp
 import biosimulators_masspy as bm
+from kisao import AlgorithmSubstitutionPolicy
+from biosimulators_utils.config import Config
 
 
 fp = "test_fixtures/Elowitz-Nature-2000-Repressilator.omex"
+# config = Config(ALGORITHM_SUBSTITUTION_POLICY=AlgorithmSubstitutionPolicy.SAME_FRAMEWORK)
+
+
+def test_pysces(config=None):
+    return bp.exec_sedml_docs_in_combine_archive(fp, "./outputs")
+
+
+def test_masspy(config=None):
+    return bm.exec_sedml_docs_in_combine_archive(fp, "./outputs")
