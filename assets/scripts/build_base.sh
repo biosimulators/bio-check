6ebbbc2c44de7e7ccb005e6c2188603933d64c1b
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-10-23 16:19:23 -0400

    update base image for refactor and cleanup

--- assets/scripts/build_base.sh
+++ assets/scripts/build_base.sh
@@ -9,12 +9,17 @@ push="$2"
 prune="$3"  # --prune
 
 if [ "$prune" == "--prune" ] || [ "$push" == "--prune" ]; then
-  docker system prune -f
+  docker system prune -f -a
 fi
 
 echo "Building base image..."
 docker build --platform linux/amd64 -f ./Dockerfile-base -t ghcr.io/biosimulators/bio-check-base:"$version" .
 echo "Built base image."
+
+echo "Tagging new base image as latest..."
+docker tag ghcr.io/biosimulators/bio-check-base:"$version" ghcr.io/biosimulators/bio-check-base:latest
+echo "New base image tagged:"
+docker images
 
 if [ "$push" == "--push" ]; then
   ./assets/scripts/push_base.sh "$version"
