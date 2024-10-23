b09ac9569c4c5fdd20e920d5987f2813580f1e03
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-10-23 15:52:33 -0400

    adjust config in test

--- compose_worker/test_new.py
+++ compose_worker/test_new.py
@@ -6,13 +6,13 @@ from kisao import AlgorithmSubstitutionPolicy
 from biosimulators_utils.config import Config
 
 
-fp = "test_fixtures/Elowitz-Nature-2000-Repressilator.omex"
-# config = Config(ALGORITHM_SUBSTITUTION_POLICY=AlgorithmSubstitutionPolicy.SAME_FRAMEWORK)
+FP = "test_fixtures/Elowitz-Nature-2000-Repressilator.omex"
+CONFIG = Config(ALGORITHM_SUBSTITUTION_POLICY=AlgorithmSubstitutionPolicy.SAME_FRAMEWORK)
 
 
 def test_pysces(config=None):
-    return bp.exec_sedml_docs_in_combine_archive(fp, "./outputs")
+    return bp.exec_sedml_docs_in_combine_archive(FP, "./outputs", config=config)
 
 
 def test_masspy(config=None):
-    return bm.exec_sedml_docs_in_combine_archive(fp, "./outputs")
+    return bm.exec_sedml_docs_in_combine_archive(FP, "./outputs", config=config)
