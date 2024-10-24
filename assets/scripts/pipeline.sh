3e58b1c4d325c38e13d455812d86455f3e009d60
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-10-23 23:32:05 -0400

    update pipeline

--- assets/scripts/pipeline.sh
+++ assets/scripts/pipeline.sh
@@ -5,19 +5,34 @@ arg2="$2"  # prune
 arg3="$3"  # deploy base after building
 arg4="$4"  # run orchestration after docker compose (up)
 
-if [ "$arg2" == "-p" ]; then
-  docker system prune -a -f
-  yes | docker buildx prune --all
-fi
+function prune_step {
+  if [ "$arg2" == "-p" ]; then
+    docker system prune -a -f
+    yes | docker buildx prune --all
+  fi
+}
 
-./assets/scripts/build_base.sh "$arg1"
-if [ "$arg3" == "-d" ]; then
-  ./assets/scripts/push_base.sh "$arg1"
-fi
+function base_step {
+  ./assets/scripts/build_base.sh "$arg1"
+  if [ "$arg3" == "-d" ]; then
+    ./assets/scripts/push_base.sh "$arg1"
+  fi
+}
 
-if [ "$arg4" == "-r" ]; then
+function services_step {
+  if [ "$arg4" == "-r" ]; then
   ./assets/scripts/build_all.sh "$arg4"
-else
-  ./assets/scripts/build_all.sh
-fi
+  else
+    ./assets/scripts/build_all.sh
+  fi
+}
+
+function run_pipeline {
+  prune_step
+  base_step
+  services_step
+}
+
+run_pipeline
+
 
