5692e0ce2f5c6b734c0c2ab71820444e60546a78
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-11-04 18:08:26 -0500

    rm lock

--- assets/docker/scripts/push_base.sh
+++ assets/docker/scripts/push_base.sh
@@ -4,10 +4,16 @@ set -e
 
 version="$1"
 gh_username="$2"
+explicit_version=1
 
 if [ "$version" == "" ]; then
-  version="$(cat ./assets/.BASE_VERSION)"
+  version="$(cat ./assets/docker/.BASE_VERSION)"
+  explicit_version=0
 fi
 
-./assets/scripts/push_image.sh "base" "$version" "$gh_username"
-echo "$version" > ./assets/.BASE_VERSION
+./assets/docker/scripts/push_image.sh "base" "$version" "$gh_username"
+
+# update base if version is specified
+if [ "$explicit_version" == 1 ]; then
+  echo "$version" > ./assets/docker/.BASE_VERSION
+fi
