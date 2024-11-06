2c09f3c0980b97f9b9c8683bbabad2e00fb2c53a
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-11-05 22:36:58 -0500

    savepoint update

--- worker/output_generator.py
+++ worker/output_generator.py
@@ -1,7 +1,8 @@
 from abc import ABC, abstractmethod
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, List
 
 import numpy as np
+from attr import dataclass
 from process_bigraph import Step, ProcessTypes
 
 from output_data import SBML_EXECUTORS, get_sbml_species_mapping
@@ -101,3 +102,98 @@ class TimeCourseOutputGenerator(OutputGenerator):
 
 
 CORE.process_registry.register('time-course-output-generator', TimeCourseOutputGenerator)
+
+
+@dataclass
+class NodeSpec:
+    name: str
+    _type: str
+    address: str
+    config: Dict[str, Any]
+    inputs: Dict[str, Any]
+    outputs: Dict[str, Any]
+
+
+@dataclass
+class StepNodeSpec(NodeSpec):
+    _type: str = "step"
+
+
+@dataclass
+class ProcessNodeSpec(NodeSpec):
+    _type: str = "process"
+
+
+def node_spec(name: str, _type: str, address: str, config: Dict[str, Any], inputs: Dict[str, Any], outputs: Dict[str, Any]):
+    return {
+        name: {
+            '_type': _type,
+            'address': address,
+            'config': config,
+            'inputs': inputs,
+            'outputs': outputs
+        }
+    }
+
+
+def step_node_spec(name: str, address: str, config: Dict[str, Any], inputs: Dict[str, Any], outputs: Dict[str, Any]):
+    return node_spec(name=name, _type="step", address=address, config=config, inputs=inputs, outputs=outputs)
+
+
+def process_node_spec(name: str, address: str, config: Dict[str, Any], inputs: Dict[str, Any], outputs: Dict[str, Any]):
+    return node_spec(name=name, _type="process", address=address, config=config, inputs=inputs, outputs=outputs)
+
+
+def time_course_node_spec(name: str, input_file: str, context: str, start_time: int, end_time: int, num_steps: int):
+    config = {
+        'input_file': input_file,
+        'start_time': start_time,
+        'end_time': end_time,
+        'num_steps': num_steps,
+        'context': context
+    }
+    return step_node_spec(
+        name=name,
+        address='time-course-output-generator',
+        config=config,
+        inputs={'parameters': [f'parameters_store_{name}']},
+        outputs={'output_data': [f'output_data_store_{name}']}
+    )
+
+
+def generate_time_course_data(input_fp: str, start: int, dur: int, steps: int, simulators: List[str] = None, expected_results_fp: str = None):
+    pass
+
+# def generate_time_course_data():
+#     doc = {
+#         'copasi': {
+#             '_type': 'step',
+#             'address': 'local:time-course-output-generator',
+#             'config': {
+#                 'input_file': TEST_SBML_FP,
+#                 'context': 'copasi',
+#                 'start_time': 0,
+#                 'end_time': 10,
+#                 'num_steps': 100
+#             },
+#             'inputs': {
+#                 'parameters': ['parameters_store']
+#             },
+#             'outputs': {
+#                 'output_data': ['output_data_store'],
+#             }
+#         }
+#     }
+#     sim = Composite({
+#             'state': doc,
+#             'emitter': {'mode': 'all'}
+#         },
+#         core=CORE
+#     )
+#     sim.save(filename="test_time_course_output_generator_before.json", outdir="./outputs")
+#     print(dir(sim))
+#     sim.run(1)
+#     results = sim.gather_results()
+#     print(f'Results:\n{results}')
+#     sim.save(filename="test_utc_output_generator_after.json", outdir="./outputs")
+#     return sim
