1dc4e06da9c9a7cc2ad18cbcd5e3c0d84b057b52
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-11-05 23:50:50 -0500

    updates

--- worker/tests.py
+++ worker/tests.py
@@ -1,9 +1,6 @@
-import uuid
 import os
-import asyncio
-from pprint import pp
+import pprint
 from dotenv import load_dotenv
-from tempfile import mkdtemp
 
 from process_bigraph import Composite
 
@@ -19,9 +16,6 @@ DB_NAME = os.getenv('DB_NAME')
 TEST_SBML_FP = "/Users/alexanderpatrie/Desktop/repos/bio-check/assets/test_fixtures/sbml-core/BorisEJB.xml"
 
 
-# db_connector = MongoDbConnector(connection_uri=MONGO_URI, database_id=DB_NAME, connector_id="test_worker")
-
-
 def test_files_worker(test_queue_index=0):
     # get first job from queue
     # job = db_connector.pending_jobs().pop(test_queue_index)
@@ -29,60 +23,17 @@ def test_files_worker(test_queue_index=0):
 
 
 def test_generate_time_course_data():
-    import pprint
-    results = generate_time_course_data(TEST_SBML_FP, 0, 1000, 5, ['copasi', 'tellurium'], out_dir="./outputs")
+    results = generate_time_course_data(
+        input_fp=TEST_SBML_FP,
+        start=0,
+        end=1000,
+        steps=5,
+        simulators=['copasi', 'tellurium'],
+        out_dir="./outputs"
+    )
     pprint.pp(results)
+
     return results
 
 
-def test_time_course_generator():
-    doc = {
-        'copasi': {
-            '_type': 'step',
-            'address': 'local:time-course-output-generator',
-            'config': {
-                'input_file': TEST_SBML_FP,
-                'context': 'copasi',
-                'start_time': 0,
-                'end_time': 10,
-                'num_steps': 100
-            },
-            'inputs': {
-                'parameters': ['parameters_store']
-            },
-            'outputs': {
-                'output_data': ['output_data_store'],
-            }
-        },
-        'emitter': {
-            '_type': 'step',
-            'address': 'local:ram-emitter',
-            'config': {
-                'emit': {
-                    'output_data': 'tree[any]'
-                }
-            },
-            'inputs': {
-                'output_data': ['output_data_store']
-            }
-        }
-    }
-
-    sim = Composite({
-            'state': doc,
-            'emitter': {'mode': 'bridge'}  # other options: bridge, ports, none, all
-        },
-        core=CORE
-    )
-
-    sim.save(filename="test_time_course_output_generator_before.json", outdir="./outputs")
-    print(dir(sim))
-    sim.run(1)
-    results = sim.gather_results()
-    print(f'Results:\n{results}')
-    sim.save(filename="test_utc_output_generator_after.json", outdir="./outputs")
-    return sim
-
-
-results = test_generate_time_course_data()
-
+TEST_RESULTS = test_generate_time_course_data()
