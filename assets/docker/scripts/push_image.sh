5daff491157278a51bc221caf5baebfb5c5c5a90
spaceBearAmadeus (Alex) <alexanderpatrie@gmail.com>
2024-11-14 12:46:32 -0500

    update push image script

--- assets/docker/scripts/push_image.sh
+++ assets/docker/scripts/push_image.sh
@@ -5,15 +5,16 @@ set -e
 lib="$1"
 version="$2"
 # gh_username="$3"
+explicit_version=1
 
 if [ "$version" == "" ]; then
   version=$(cat "$lib"/.VERSION)
+  explicit_version=0
 fi
 
 # push version to GHCR
 docker tag bio-compose-server-"$lib":"$version" ghcr.io/biosimulators/bio-compose-server-"$lib":"$version"
 docker push ghcr.io/biosimulators/bio-compose-server-"$lib":"$version"
-
 
 # push newest latest to GHCR
 docker tag ghcr.io/biosimulators/bio-compose-server-"$lib":"$version" ghcr.io/biosimulators/bio-compose-server-"$lib":latest
@@ -26,7 +27,9 @@ else
   VERSION_FILE=./"$lib"/.VERSION
 fi
 
-echo "$version" > "$VERSION_FILE"
+if [ "$explicit_version" == 1 ]; then
+  echo "Updating internal version of $lib with specified version."
+  echo "$version" > "$VERSION_FILE"
+fi
 
-# Optional: Output the new version
 echo "Updated version: $version"
